f421d0e6b8da5e0a4b627b48f67f0328bd4535c7
kartofchence <magi08010522@gmail.com>
2022-03-12 15:18:33 +0200

    share knowleadge

--- Share.py
+++ Share.py
@@ -1,8 +1,9 @@
 import pygame
+import pygame_textinput
 import static
 import button
 
-from PIL import Image, ImageFont, ImageDraw
+pygame.init()
 
 SCREEN_HEIGHT = 800
 SCREEN_WIDTH = 1535
@@ -11,12 +12,27 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 background = pygame.image.load("imgs/background.png")
 
 back_bttn = pygame.image.load('imgs_updated/Back.png').convert_alpha()
-
+share_bttn = pygame.image.load('imgs_updated/IfYouHavnetSeen.png').convert_alpha()
+type_space = pygame.image.load('imgs_updated/OriginalButton.png').convert_alpha()
 
 back = button.Button(10, 10, back_bttn, 150, 100)
+share = static.Static(375, 50, share_bttn, 800, 200)
+type = static.Static(375, 270, type_space, 800 ,500)
+
+textinput = pygame_textinput.TextInputVisualizer()
+
+base_font = pygame.font.Font('font/ChalkboardSE-Regular.ttf', 32)
 
 while True:
 	screen.blit(background, (0, 0))
+
+	share.draw(screen)
+	type.draw(screen)
+
+	events = pygame.event.get()
+
+	textinput.update(events)
+	screen.blit(textinput.surface, (480, 375))
 
 	switch_file = 0
 
@@ -25,11 +41,11 @@ while True:
 		break
 
 	for event in pygame.event.get():
-
 		if event.type == pygame.QUIT:
 			exit()
 
 	pygame.display.update()
+
 
 if switch_file == 1:
 	import main
