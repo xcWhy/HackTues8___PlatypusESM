e17f066e2e03f9b2d3893f6006ee493e75c67b20
kartofchence <magi08010522@gmail.com>
2022-03-12 16:17:43 +0200

    settings and homepg

--- homepg.py
+++ homepg.py
@@ -1,26 +1,24 @@
 import pygame
 import button
+import static
 
 SCREEN_HEIGHT = 800
 SCREEN_WIDTH = 1535
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-
-background = pygame.image.load("imgs/background.png")
-
+background = pygame.image.load("imgs_updated/Start_Screen.png").convert_alpha()
 set_bttn = pygame.image.load('imgs_updated/Settings.png').convert_alpha()
 
+bckg = static.Static(0, -100, background, 1535, 900)
 set = button.Button(10, 10, set_bttn, 150, 100)
 
-
-
 while True:
-    screen.blit(background, (0, 0))
-
+    #screen.blit(background, (0, 0))
+    bckg.draw(screen)
     switch_file = 0
 
-    if back.draw(screen):
+    if set.draw(screen):
         switch_file = 1
         break
 
