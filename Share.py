a6dce60a6a56bc105293f34ead867b0469397a4f
kartofchence <magi08010522@gmail.com>
2022-03-12 13:20:23 +0200

    gebeasfnsf

--- Share.py
+++ Share.py
@@ -11,13 +11,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 background = pygame.image.load("imgs/background.png")
 
 back_bttn = pygame.image.load('imgs_updated/Back.png').convert_alpha()
-Or_button = Image.open("OriginalButton.png")
-font = ImageFont.truetype('ChalkboardSE-Regular.ttf', 100)
 
-share_static = "If you know anything that you haven't seen in our training program and it might be useful, please add it :)"
-share_editable = ImageDraw.Draw(Or_button)
-share_editable.text((-25, -80), share_static, (F, F, F), font = font)
-Or_button.draw(screen)
 
 back = button.Button(10, 10, back_bttn, 150, 100)
 
