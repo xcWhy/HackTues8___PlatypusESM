482fe65defe513f9afb456fc78749c1eea80c418
kartofchence <magi08010522@gmail.com>
2022-03-13 06:31:47 +0200

    ardtcdvb cfgv

--- multi_player.py
+++ multi_player.py
@@ -10,6 +10,9 @@ background = pygame.image.load("imgs/background.png")
 back_bttn = pygame.image.load('imgs_updated/Back.png').convert_alpha()
 
 bttn = pygame.image.load('imgs_updated/OriginalButton.png').convert_alpha()
+cum = pygame.image.load('imgs_updated/ComingSoon.png').convert_alpha()
+
+cum = static.Static(400, -60, cum, 700, 900)
 back = button.Button(10, 690, bttn, 150, 100)
 
 def multi_player_func(new_start):
@@ -17,6 +20,8 @@ def multi_player_func(new_start):
 
     if back.draw(screen):
         new_start = 3
+    cum.draw(screen)
+    draw("Back", 30, 710, 36)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
